4ac0efaa09b1442fa6fa83c9a5194902c9acbaa2
Collid3 <masolathabangjerry11@gmail.com>
2023-08-28 14:15:14 +0200

    reverse back to my name in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer>MusicPad &copy; Copyright 2023. Developed by Mulan Alaia</footer>
+    <footer>MusicPad &copy; Copyright 2023. Developed by Thabang Masola</footer>
   );
 };
 
